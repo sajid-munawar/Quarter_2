e016a664ab9ca8549576074baf107bd3035cb728
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-27 01:30:09 +0500

    task 11

--- Assignment_2/PIAIC142475_Assignment2.py
+++ Assignment_2/PIAIC142475_Assignment2.py
@@ -186,7 +186,8 @@ def function11():
     # Replace all odd numbers in arr with -1 without changing arr.
     
     arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    ans = #write your code here 
+    ans =arr[:].copy() #write your code here 
+    ans[ans%2!=0]=-1
   
     return ans
 
