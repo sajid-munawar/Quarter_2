f9690a6593c088ce5f89d9686d33e0201058c66b
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-27 08:55:41 +0500

    task 15 solved

--- Assignment_2/PIAIC142475_Assignment2.py
+++ Assignment_2/PIAIC142475_Assignment2.py
@@ -251,7 +251,7 @@ def function15():
     
     
     arr = np.array([[ 8,  2, -2],[-4,  1,  7],[ 6,  3,  9]])
-    ans = #write your code here 
+    ans =arr[np.argsort(arr[:, 1])] #write your code here 
   
     return ans
 
